0058b5ae917bc48688241e9504f7df668bb24a88
anubhabreveals <61702730+anubhabreveals@users.noreply.github.com>
2022-10-24 19:21:39 +0530

    completed

--- snake_game.py
+++ snake_game.py
@@ -4,6 +4,11 @@ import random
 
 delay = 0.1
 
+# Score
+score = 0
+high_score = 0
+
+
 # Set up the screen
 
 wn = turtle.Screen()
@@ -31,22 +36,36 @@ food.shape("circle")
 food.color("yellow")
 food.penup()
 food.goto(0,100)
-food.direction = "stop"
 
+segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("score: 0 High Score: 0", align="center", font=("courier", 24, "normal"))
 
 # Function
 
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 def move():
     if head.direction == "up":
@@ -79,14 +98,94 @@ wn.onkeypress(go_right, "d")
 
 while True:
     wn.update()
+    
+    # check for collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
 
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000,1000)
+
+        # Clear the segments list
+        segments.clear()
+
+        # Reset score
+
+        score = 0
+        
+        # reset the delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("score: {} High Score: {}".format(score, high_score), align="center" ,font=("Courier", 24, "normal"))
+
+
+    #Check for a collision with the food
     if head.distance(food) < 20:
         # Move the food to random coordinates
         x = random.randint(-290,290)
         y = random.randint(-290,290)
         food.goto(x,y)
 
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+
+        # Shorten the delay
+        delay -= 0.005
+        # Increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        pen.clear()
+        pen.write("score: {} High Score: {}".format(score, high_score), align="center" ,font=("Courier", 24, "normal"))
+
+    # Move the end segments first inn reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x,y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
     move()
+
+    # Check for self collision
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000,1000)
+
+            # Clear the segments list
+            segments.clear()
+
+            # Reset score
+
+            score = 0
+            # reset the delay
+            delay = 0.1
+
+            pen.clear()
+            pen.write("score: {} High Score: {}".format(score, high_score), align="center" ,font=("Courier", 24, "normal"))
+
+
 
     time.sleep(delay)
 
