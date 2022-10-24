d1466c420240d1c8abcb8a94cd93bc264072c248
anubhabreveals <61702730+anubhabreveals@users.noreply.github.com>
2022-10-24 17:13:08 +0530

    part 2

---
+++ snake_game.py
@@ -0,0 +1,75 @@
+import turtle
+import time
+
+delay = 0.1
+
+# Set up the screen
+
+wn = turtle.Screen()
+wn.title("Snake Game by @AnubhabMajumder")
+wn.bgcolor("blue")
+wn.setup(width=600, height=600)
+wn.tracer(0)  # Turns off the screen update
+
+# Snake head
+
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+
+# Function
+
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_left():
+    head.direction = "left"
+
+def go_right():
+    head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+
+# keyboard bindings
+
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
+
+# Main game loop
+
+while True:
+    wn.update()
+
+    move()
+
+    time.sleep(delay)
+
+wn.mainloop()
